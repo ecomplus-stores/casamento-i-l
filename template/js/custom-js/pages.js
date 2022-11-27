fabc129d4f36ee191f39fae332d1339d352972f2
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2022-11-27 12:43:10 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -5,6 +5,7 @@ document.getElementById('c-62e695eaeb859112169ed48d').href = '#os-noivos'
 document.getElementById('c-62e69edfeb859112169edb46').href = '#os-padrinhos'
 document.getElementById('c-62e69f87eb859112169edbea').href = '#o-local'
 document.getElementById('c-62c9df90eb859112167f9c39').target = '_blank'
+document.getElementById('c-630036c7f8d4267de7105fc6').target = '/pages/dicas-do-casal'
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {

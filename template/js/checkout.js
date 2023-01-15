2d435699047d8a44ea83b11fff2702854db67983
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-01-15 20:27:45 -0300

    Update checkout.js

--- template/js/checkout.js
+++ template/js/checkout.js
@@ -1,2 +1,29 @@
 import '#template/js/checkout'
 import './custom-js/checkout'
+
+const routerToLink = window.storefrontApp && window.storefrontApp.router
+routerToLink.afterEach(({ name }) => {
+  if(name === 'confirmation') {
+    const appendLink = () => {
+      let locale
+      let currency
+      const $html = document.querySelector('.order-info__redirect .btn.btn-lg.btn-success')
+      const local = Intl.DateTimeFormat().resolvedOptions().timeZone
+      if (local === 'America/Sao_Paulo') {
+        locale = 'BR'
+        currency = 'BRL'
+      } else {
+        locale = 'US'
+        currency = 'USD'
+      }
+      if ($html) {
+        const atribute = $html.href.replace(',00', '').replace('lc=US', `lc=${locale}`).replace('currency_code=USD', `currency_code=${currency}`)
+        $html.href = atribute
+        clearInterval(tryAppendInterval)
+        
+        
+      }
+    }
+    const tryAppendInterval = setInterval(appendLink, 200)
+  }
+})
